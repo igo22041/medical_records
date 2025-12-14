252b73141beece3219d465b241a3c3d597ca0fb5
igo22041 <ignatusovic@gmail.com>
2025-12-14 21:52:55 +0300

    database updt

--- config/env.php
+++ config/env.php
@@ -26,13 +26,13 @@ function env($key, $default = null) {
     return $value;
 }
 
-// Определяем переменные окружения для базы данных
+
 // Railway использует префикс MYSQL_, но мы также поддерживаем DB_ для гибкости
-define('DB_HOST', env('MYSQL_HOST', env('DB_HOST', 'localhost')));
+define('DB_HOST', env('MYSQL_HOST', env('DB_HOST', 'switchback.proxy.rlwy.net')));
 define('DB_NAME', env('MYSQL_DATABASE', env('DB_NAME', 'medical_records')));
 define('DB_USER', env('MYSQL_USER', env('DB_USER', 'root')));
-define('DB_PASS', env('MYSQL_PASSWORD', env('DB_PASSWORD', '')));
-define('DB_PORT', env('MYSQL_PORT', env('DB_PORT', '3306')));
+define('DB_PASS', env('MYSQL_PASSWORD', env('DB_PASSWORD', 'sBxNvZMqCrmjjXmbuatiSsPBPwDEDuzW')));
+define('DB_PORT', env('MYSQL_PORT', env('DB_PORT', '21778')));
 
 // Другие переменные окружения
 define('APP_ENV', env('APP_ENV', 'production'));
